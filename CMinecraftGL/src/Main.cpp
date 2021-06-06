c282e07f207416c5fd2bb543640ecc91a2821b58
Jukitsu <ninjax2312@gmail.com>
2021-06-06 14:41:05 +0200

    Ajoutez des fichiers projet.

---
+++ CMinecraftGL/src/Main.cpp
@@ -0,0 +1,91 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GL/glu.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include "shader.h"
+
+
+GLfloat vertices[12] = {
+    -0.5, 0.5, 1.0,
+    -0.5, -0.5, 1.0,
+    0.5, -0.5, 1.0,
+    0.5, 0.5, 1.0
+};
+
+GLuint indices[6] = {
+    0, 1, 2,
+    0, 2, 3
+};
+
+
+void send_data_to_gpu() {
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(float), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+    GLuint ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(int), indices, GL_STATIC_DRAW);
+}
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+    }
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    send_data_to_gpu();
+    std::string vertex_shader_source = ParseShader("res/shaders/vertex_shader.glsl");
+    std::string fragment_shader_source = ParseShader("res/shaders/fragment_shader.glsl");
+    GLuint shader_program = create_shader(vertex_shader_source, fragment_shader_source);
+
+    glUseProgram(shader_program);
+
+
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
